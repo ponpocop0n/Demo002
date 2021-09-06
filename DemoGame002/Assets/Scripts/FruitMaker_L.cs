4ec2deab7a031a27ed736f15bbaf64941914af26
ponpocop0n <65337415+ponpocop0n@users.noreply.github.com>
2021-09-07 01:25:21 +0900

    GetFruit_1
    フルーツが1秒ごとに出てきます

---
+++ DemoGame002/Assets/Scripts/FruitMaker_L.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitMaker_L : MonoBehaviour
+{
+    float zikan2;
+    public GameObject[] frt;
+    int frtnum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zikan2 = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.fixedTime - zikan2 > 1f)
+        {
+            frtnum = Random.Range(0, 7);
+            Instantiate(frt[frtnum], new Vector3(-1f, 0f, 5f), Quaternion.identity);
+            zikan2 = Time.fixedTime;
+        }
+    }
+}
